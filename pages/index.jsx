a5107e43280bcf3e151fb0c97c3cc600a2eec6cc
KunalBishwal <159259649+KunalBishwal@users.noreply.github.com>
2024-10-15 00:46:51 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,15 +19,15 @@ export default function HomePage() {
           <div className={styles.content}>
             <h1 className={styles.name}>CODExKUN</h1>
             <h6 className={styles.bio}>
-              Btech Student in
-              <span className={styles.react}> DataScience.</span>
+              Btech Student In
+              <span className={styles.react}> Data Science.</span>
             </h6>
 
             {/* Skill Icons Section */}
             <div className={styling.skillsContainer}>
               {/* Skill item for React */}
               <div className={styling.skillItem}>
-                <FontAwesomeIcon icon={faReact} size="3x" className={`${styling.icon} ${styling.reactIcon}`} />
+                <FontAwesomeIcon icon={faReact} size="2x" className={`${styling.icon} ${styling.reactIcon}`} />
                 <div className={styling.skillText}>React</div>
               </div>
 
@@ -39,31 +39,31 @@ export default function HomePage() {
 
               {/* Skill item for Python */}
               <div className={styling.skillItem}>
-                <FontAwesomeIcon icon={faPython} size="3x" className={`${styling.icon} ${styling.pythonIcon}`} />
+                <FontAwesomeIcon icon={faPython} size="2x" className={`${styling.icon} ${styling.pythonIcon}`} />
                 <div className={styling.skillText}>Python</div>
               </div>
 
               {/* Skill item for Java */}
               <div className={styling.skillItem}>
-                <FontAwesomeIcon icon={faJava} size="3x" className={`${styling.icon} ${styling.javaIcon}`} />
+                <FontAwesomeIcon icon={faJava} size="2x" className={`${styling.icon} ${styling.javaIcon}`} />
                 <div className={styling.skillText}>Java</div>
               </div>
 
               {/* Skill item for C */}
               <div className={styling.skillItem}>
-                <FontAwesomeIcon icon={faC} size="4x" className={`${styling.icon} ${styling.cIcon}`} />
+                <FontAwesomeIcon icon={faC} size="2x" className={`${styling.icon} ${styling.cIcon}`} />
                 <div className={styling.skillText}>C</div>
               </div>
 
               {/* Skill item for C++ (Using faCode as a substitute) */}
               <div className={styling.skillItem}>
-                <FontAwesomeIcon icon={faCode} size="4x" className={`${styling.icon} ${styling.cppIcon}`} />
+                <FontAwesomeIcon icon={faCode} size="2x" className={`${styling.icon} ${styling.cppIcon}`} />
                 <div className={styling.skillText}>C++</div>
               </div>
 
               {/* Skill item for MySQL */}
               <div className={styling.skillItem}>
-                <FontAwesomeIcon icon={faDatabase} size="3x" className={`${styling.icon} ${styling.mysqlIcon}`} />
+                <FontAwesomeIcon icon={faDatabase} size="2x" className={`${styling.icon} ${styling.mysqlIcon}`} />
                 <div className={styling.skillText}>MySQL</div>
               </div>
             </div>
@@ -79,7 +79,6 @@ export default function HomePage() {
             </div>
           </div>
 
-          {}
           <div className={styling.imageWrapper}>
             <Image src={logo} alt="CODExKUN Logo" className={styling.myImg} priority />
           </div>
