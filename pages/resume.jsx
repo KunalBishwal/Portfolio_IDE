b937889d8ee8ef0de2ea703d4269c0e42ea3421f
KunalBishwal <159259649+KunalBishwal@users.noreply.github.com>
2024-10-22 02:02:09 +0530

    Update resume.jsx

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -8,11 +8,11 @@ const ContactPage = () => {
 
         <div className={styles.item}>
           <h4 className={styles.resumeHead}>BTech Student</h4>
-          <div>SRM UNIVERSITY</div>
-          <p>These is the list of my CGPA I got till now including 1st and 2nd sem</p>
-          <ul>
-            <li>8.8</li>
-            <li>9.8</li>
+          <div>SRM University</div>
+          <p>These is the list of my CGPA I got till now</p>
+          <ul type='circle'>
+            <li>8.8 in 1st sem</li>
+            <li>9.8 in 2nd sem</li>
           </ul>
           <div>2023 - Present</div>
         </div>
@@ -38,44 +38,6 @@ const ContactPage = () => {
       {/* Work Experience Section */}
       <div className={styles['work-section']}>
         <h3 className={styles.heading}>Professional Experience</h3>
-
-        {/* Uncomment this block to display work experience */}
-        {/*
-        <div className={styles.item}>
-          <h4 className={styles.resumeHead}>Virtuous Tech Enterprise</h4>
-          <div>Remote: Edo State, Nigeria</div>
-          <div>Frontend Developer with React</div>
-          <div>2024 - Present</div>
-        </div>
-
-        <div className={styles.item}>
-          <h4 className={styles.resumeHead}>SKI MUSIC</h4>
-          <div>Remote: Lagos State, Nigeria</div>
-          <div>Lead Mobile App Developer</div>
-          <div>2024 - Present</div>
-        </div>
-
-        <div className={styles.item}>
-          <h4 className={styles.resumeHead}>BIG Foundation (MY GRIND APP)</h4>
-          <div>Remote: Lagos State, Nigeria</div>
-          <div>Lead Developer</div>
-          <div>2023</div>
-        </div>
-
-        <div className={styles.item}>
-          <h4 className={styles.resumeHead}>SOUTECH VENTURES</h4>
-          <div>Physical: Imo State, Nigeria</div>
-          <div>Web Developer & Instructor</div>
-          <div>2023</div>
-        </div>
-
-        <div className={styles.item}>
-          <h4 className={styles.resumeHead}>KRIS MEDICAL LABORATORY</h4>
-          <div>Physical: Imo State, Nigeria</div>
-          <div>Laboratory Scientist Intern</div>
-          <div>2019</div>
-        </div>
-        */}
       </div>
     </div>
   );
